6bbfe810fe0529de64d47b5828bfbbffde7acc0b
chanel5er <84465122+chanel5er@users.noreply.github.com>
2022-08-31 21:30:49 +0200

    Update and rename ZonesController.cs to PutDeleteZonesController.cs

--- Controllers/PutDeleteZonesController.cs
+++ Controllers/PutDeleteZonesController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,41 +11,20 @@ namespace API_Project2.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ZonesController : ControllerBase
+    public class PutDeleteZonesController : ControllerBase
     {
         private readonly ConnectedOfficeContext _context;
 
-        public ZonesController(ConnectedOfficeContext context)
+        public PutDeleteZonesController(ConnectedOfficeContext context)
         {
             _context = context;
-        }
-
-        // GET: api/Zones
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Zone>>> GetZone()
-        {
-            return await _context.Zone.ToListAsync();
-        }
-
-        // GET: api/Zones/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Zone>> GetZone(Guid id)
-        {
-            var zone = await _context.Zone.FindAsync(id);
-
-            if (zone == null)
-            {
-                return NotFound();
-            }
-
-            return zone;
         }
 
         // PUT: api/Zones/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutZone(Guid id, Zone zone)
+        public async Task<IActionResult> PutZone(Guid id, ZoneModel zone)
         {
             if (id != zone.ZoneId)
             {
@@ -69,39 +48,12 @@ namespace API_Project2.Controllers
                     throw;
                 }
             }
-
             return NoContent();
-        }
-
-        // POST: api/Zones
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPost]
-        public async Task<ActionResult<Zone>> PostZone(Zone zone)
-        {
-            _context.Zone.Add(zone);
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateException)
-            {
-                if (ZoneExists(zone.ZoneId))
-                {
-                    return Conflict();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return CreatedAtAction("GetZone", new { id = zone.ZoneId }, zone);
         }
 
         // DELETE: api/Zones/5
         [HttpDelete("{id}")]
-        public async Task<ActionResult<Zone>> DeleteZone(Guid id)
+        public async Task<ActionResult<ZoneModel>> DeleteZone(Guid id)
         {
             var zone = await _context.Zone.FindAsync(id);
             if (zone == null)
